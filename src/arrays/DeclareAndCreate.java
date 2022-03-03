5ddd88eb6388423778445abf742f7a6c00386f7e
EmreCanCamoglu <can.camoglu@metu.edu.tr>
2022-03-03 16:22:41 +0300

    Unfinished job

---
+++ src/arrays/DeclareAndCreate.java
@@ -0,0 +1,21 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class DeclareAndCreate {
+    public static void main(String[] args) {
+
+
+        int[][] values = new int[2][3];
+
+        values[0] = new int[]{8, 20, 50};
+        values[1] = new int[]{12, 30, 75};
+
+        System.out.println(Arrays.deepToString(values));
+
+
+        ////!!!!!!!!!LEARN HOW ITS TRULY DONE!!!!!!!!!
+
+
+    }
+}
