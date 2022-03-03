61777e0696371043417888133a3a0337711f124c
EmreCanCamoglu <can.camoglu@metu.edu.tr>
2022-03-03 17:22:09 +0300

    given an integer, finds its factorial value

---
+++ src/loops/Factorial.java
@@ -0,0 +1,20 @@
+package loops;
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        long factorial = 1;
+
+        while (n > 0) {
+            factorial *= n;
+            n--;
+
+        }
+        System.out.println(factorial);
+
+
+    }
+}
