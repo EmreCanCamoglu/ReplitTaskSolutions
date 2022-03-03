027260d603a6d19b95ae00d204a6b5406c71a567
EmreCanCamoglu <can.camoglu@metu.edu.tr>
2022-03-03 16:51:47 +0300

    Prints the elements by groups of two's.

---
+++ src/arrays/Printing02.java
@@ -0,0 +1,26 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class Printing02 {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        String[] arr = new String[8];
+
+        for (int i = 0; i < 8; i++) {
+
+            arr[i] = input.nextLine();
+
+        }
+
+        for (int i = 0; i < 8; i += 2) {
+
+            System.out.println(arr[i] + ", " + arr[i + 1]);
+
+        }
+
+
+    }
+}
