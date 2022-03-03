3bc88e0f687dfffa4e31f084f41b760e599ca4ef
EmreCanCamoglu <can.camoglu@metu.edu.tr>
2022-03-03 16:34:57 +0300

    Oscar task

---
+++ src/strings/OscarTask1.java
@@ -0,0 +1,25 @@
+package strings;
+
+import java.util.Scanner;
+
+public class OscarTask1 {
+
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        int n = s.nextInt();
+
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+
+                System.out.print(i);
+
+            }
+            System.out.println();
+        }
+
+
+    }
+}
