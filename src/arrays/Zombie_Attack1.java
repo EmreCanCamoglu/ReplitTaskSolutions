b764c99b373d55c69aaf5f414444b1b72cd47a83
EmreCanCamoglu <can.camoglu@metu.edu.tr>
2022-03-03 17:01:57 +0300

    THERE IS A ZOMBIE OUTBREAK!!!

---
+++ src/arrays/Zombie_Attack1.java
@@ -0,0 +1,40 @@
+package arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Zombie_Attack1 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int[] inhabitants = {input.nextInt(), input.nextInt(), input.nextInt(), input.nextInt(), input.nextInt(), input.nextInt(), input.nextInt(), input.nextInt()};
+
+        boolean b = true;
+        int i;
+        int h = 0;
+        int a;
+
+        do {
+            i = 0;
+            System.out.println("Day " + h + " " + Arrays.toString(inhabitants));
+            for (int j = 0; j < 8; j++) {
+                a = inhabitants[j];
+                inhabitants[j] /= 2;
+                if (a / 2 != 0)
+                    ++i;
+            }
+            h++;
+            if (i == 0) {
+                System.out.println("Day " + h + " " + Arrays.toString(inhabitants));
+                System.out.println("---- EXTINCT ----");
+                b = false;
+            }
+
+        } while (b);
+
+
+    }
+
+
+}
+
