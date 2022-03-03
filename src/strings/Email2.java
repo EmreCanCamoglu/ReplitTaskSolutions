2964d4aa9964d250ec2ad3e1b8b9ed0fc5220877
EmreCanCamoglu <can.camoglu@metu.edu.tr>
2022-03-03 16:25:14 +0300

    impr. ver. of Email1

---
+++ src/strings/Email2.java
@@ -0,0 +1,18 @@
+package strings;
+
+import java.util.Scanner;
+
+public class Email2 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String email = scan.next();
+
+
+        System.out.println("First name: " + email.substring(0, 1).toUpperCase() + email.substring(1, email.indexOf("_")));
+        System.out.println("Last name: " + email.substring(email.indexOf("_") + 1, email.indexOf("_") + 2).toUpperCase() + email.substring(email.indexOf("_") + 2, email.indexOf("@")));
+        System.out.println("Domain: " + email.substring(email.indexOf("@") + 1, email.indexOf(".")));
+
+
+    }
+}
