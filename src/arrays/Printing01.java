f76a7b69901224c1d1c8ef3e696c7e21db4135e3
EmreCanCamoglu <can.camoglu@metu.edu.tr>
2022-03-03 16:36:31 +0300

    Prints first 3 chars of each string given

---
+++ src/arrays/Printing01.java
@@ -0,0 +1,23 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class Printing01 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+
+        String[] arr = new String[5];
+        for (int i = 0; i < 5; i++) {
+            arr[i] = scan.nextLine();
+        }
+
+
+        for (String s : arr) {
+            System.out.println(s.substring(0, 3));
+        }
+
+
+    }
+
+}
