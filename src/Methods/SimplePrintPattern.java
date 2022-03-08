ad3e90f3c7d6b0694fb0bbfdc41d4cb22f9bda49
EmreCanCamoglu <can.camoglu@metu.edu.tr>
2022-03-08 09:44:34 +0300

    basic

---
+++ src/Methods/SimplePrintPattern.java
@@ -0,0 +1,18 @@
+package Methods;
+
+public class SimplePrintPattern {
+
+    public static void printHollowRect() {
+
+        System.out.println("*****");
+        System.out.println("*   *");
+        System.out.println("*   *");
+        System.out.println("*   *");
+        System.out.println("*****");
+
+    }
+
+    public static void main(String[] args) {
+        printHollowRect();
+    }
+}
