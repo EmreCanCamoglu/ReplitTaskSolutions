8dd6a785babf715a87cc3ceda0797a87e9745d9e
EmreCanCamoglu <can.camoglu@metu.edu.tr>
2022-03-09 20:17:26 +0300

    Reverses the str

---
+++ src/Methods/MethodsWithString_ReverseString.java
@@ -0,0 +1,18 @@
+package Methods;
+
+import java.util.Scanner;
+
+public class MethodsWithString_ReverseString {
+    public static String reverse(String input) {
+        String result = "";
+        for (int i = 0; i < input.length(); i++) {
+            result += input.charAt(input.length() - 1 - i) + "";
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println(reverse(in.next()));
+    }
+}
