1b82de42f80713d0bed62a09e979bc653fa4cd39
EmreCanCamoglu <can.camoglu@metu.edu.tr>
2022-03-09 14:26:40 +0300

    Water bill calc.

---
+++ src/Methods/MethodsWithReturn_WaterTax.java
@@ -0,0 +1,25 @@
+package Methods;
+
+import java.util.Scanner;
+
+public class MethodsWithReturn_WaterTax {
+    public static double waterTax(double units) {
+        double bill = 0.0;
+
+        if (units <= 50)
+            bill = units * 0.6;
+        else if (units <= 100)
+            bill = units * 0.9;
+        else if (units <= 150)
+            bill = units * 0.9 + 50;
+        else
+            bill = units * 0.9 + 100;
+
+        return bill;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println(waterTax(in.nextDouble()));
+    }
+}
