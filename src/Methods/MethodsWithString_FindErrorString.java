bca43a9a79d644bd581a93d1ceb1c08301c66399
EmreCanCamoglu <can.camoglu@metu.edu.tr>
2022-03-09 18:42:19 +0300

    Finds if the given string starts with "error" or not

---
+++ src/Methods/MethodsWithString_FindErrorString.java
@@ -0,0 +1,16 @@
+package Methods;
+
+import java.util.Scanner;
+
+public class MethodsWithString_FindErrorString {
+    public static boolean isError(String line) {
+
+        return line.startsWith("error");
+    }
+
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println(isError(in.nextLine()));
+    }
+}
