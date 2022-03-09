b21de33fa9badae7b5b049143d54c236b6a75df5
EmreCanCamoglu <can.camoglu@metu.edu.tr>
2022-03-09 18:16:11 +0300

    Returns the clean version of text

---
+++ src/Methods/MethodsWithString_CleanString.java
@@ -0,0 +1,19 @@
+package Methods;
+
+import java.util.Scanner;
+
+public class MethodsWithString_CleanString {
+    public static String clean(String text, String badWord) {
+
+        text = text.replace(badWord, "");
+        text = text.replace("  ", " ");
+        text = text.trim();
+        return text;
+
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println(clean(in.nextLine(), in.nextLine()));
+    }
+}
